a4f2a3372cefa385bf462c902fd2a0ff1004b48d
gongzhaopeng <gongguochuanqi@gmail.com>
2021-09-03 11:55:38 +0800

    ...

--- src/Resume.js
+++ src/Resume.js
@@ -1,42 +1,55 @@
-import React from "react";
+import React from 'react';
 
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PhoneIcon from '@material-ui/icons/Phone';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+
 import {makeStyles} from '@material-ui/core/styles'
+import {green} from '@material-ui/core/colors';
 
 import Profile from "./Profile";
 
-import './Resume.css'
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        maxWidth: 500,
+        margin: 'auto'
+    },
+});
 
 function Resume() {
+
+    const classes = useStyles();
+    const [tabIndex, setTabIndex] = React.useState(0);
+
+    const handleTabChange = (event, newTabIndex) => {
+        setTabIndex(newTabIndex);
+    };
 
     return (
         <React.Fragment>
             <CssBaseline/>
-            <Container style={{
-                backgroundColor: 'lightblue'
-            }}>
-                <Profile/>
+            <Container style={{backgroundColor: green[100]}}>
+                <Paper square className={classes.root}>
+                    <Tabs
+                        value={tabIndex}
+                        onChange={handleTabChange}
+                        variant="fullWidth"
+                        indicatorColor="secondary"
+                        textColor="secondary"
+                        aria-label="icon label tabs example"
+                    >
+                        <Tab icon={<PhoneIcon/>} label="RECENTS"/>
+                        <Tab icon={<FavoriteIcon/>} label="FAVORITES"/>
+                        <Tab icon={<PersonPinIcon/>} label="NEARBY"/>
+                    </Tabs>
+                </Paper>
             </Container>
-            <Container style={{
-                width: '100%',
-                minHeight: '500px',
-                boxAlign: 'center',
-                backgroundColor: 'grey',
-            }}>
-                <Typography style={{textAlign: 'center', backgroundColor: 'pink'}}>
-                    HERE IS MY PLAYGROUND.
-                </Typography>
-                <Typography style={{
-                    textAlign: 'center',
-                    minHeight: '60px',
-                    backgroundColor: 'blueviolet'
-                }}>
-                    WELCOME.
-                </Typography>
-            </Container>
-            {/*TODO*/}
         </React.Fragment>
     )
 }
