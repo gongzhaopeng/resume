4f02e86bf6c40a694fb034758cc3975c1944cd94
gongzhaopeng <gongguochuanqi@gmail.com>
2021-09-09 18:27:41 +0800

    ...

---
+++ src/components/SkillItemChip.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Chip from '@material-ui/core/Chip'
+
+export function SkillItemChip({skillIcon, skillName, color}) {
+
+    return <Chip
+        // variant="outlined"
+        size="small"
+        icon={skillIcon}
+        label={skillName}
+        clickable
+        color={color}
+        style={{margin: 10}}
+    />
+}
+
+export default SkillItemChip
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,21 +2,61 @@ import React, {Fragment} from 'react';
 
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import EmojiSymbolsIcon from '@material-ui/icons/EmojiSymbols';
+import CloudDoneIcon from '@material-ui/icons/CloudDone';
+import CropIcon from '@material-ui/icons/Crop';
+import SvgIcon from '@material-ui/core/SvgIcon'
 
 import {makeStyles} from '@material-ui/core/styles';
 import {cyan} from '@material-ui/core/colors';
 
 import UniformCard from './UniformCard'
+import SkillItemChip from './SkillItemChip'
+
+import springLogo from '../assets/images/logos/spring-logo.svg'
+
+const useStyles = makeStyles(theme => ({
+    skillItemChips: {
+        display: 'flex',
+        flexWrap: "wrap",
+        justifyContent: 'center'
+    }
+}));
 
 function Skills() {
+
+    const {skillItemChips} = useStyles()
 
     return (
         <Fragment>
             <UniformCard style={{backgroundColor: cyan[300], height: 200}}>
-                <CardContent>
-                    <Typography>
-                        Java is all around!
-                    </Typography>
+                <CardContent className={skillItemChips}>
+                    <SkillItemChip
+                        skillIcon={<EmojiSymbolsIcon/>}
+                        skillName='Microservice Architecture'
+                        color='secondary'
+                    />
+                    <SkillItemChip
+                        skillIcon={<CloudDoneIcon/>}
+                        skillName='Kubernetes'
+                        color='primary'
+                    />
+                    <SkillItemChip
+                        skillIcon={<CropIcon/>}
+                        skillName='Node.js'
+                        color='default'
+                    />
+                    <SkillItemChip
+                        skillIcon={<CloudDoneIcon/>}
+                        // skillIcon={<SvgIcon component={springLogo}/>}
+                        skillName='Spring Framework'
+                        color='primary'
+                    />
+                    <SkillItemChip
+                        skillIcon={<EmojiSymbolsIcon/>}
+                        skillName='Spring Cloud'
+                        color='secondary'
+                    />
                 </CardContent>
             </UniformCard>
             <UniformCard style={{backgroundColor: cyan[100], height: 200}}>
