8a95e6f138dd02a696a0e153624d2910489a81e2
gongzhaopeng <gongguochuanqi@gmail.com>
2021-09-10 15:06:38 +0800

    ...

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
 
+import {shuffler} from 'd3-array'
+import {randomLcg} from 'd3-random'
+
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import EmojiSymbolsIcon from '@material-ui/icons/EmojiSymbols';
 import CloudDoneIcon from '@material-ui/icons/CloudDone';
 import CropIcon from '@material-ui/icons/Crop';
+import Avatar from '@material-ui/core/Avatar'
 import SvgIcon from '@material-ui/core/SvgIcon'
 
 import {makeStyles} from '@material-ui/core/styles';
@@ -15,48 +19,49 @@ import SkillItemChip from './SkillItemChip'
 
 import {ReactComponent as SpringLogo} from '../assets/images/logos/spring-logo.svg'
 
+const skillsData = [
+    {name: 'Microservice Architecture', color: 'secondary', logo: null},
+    {name: 'Kubernetes', color: 'primary', logo: <CloudDoneIcon/>},
+    {name: 'Node.js', color: 'default', logo: <CropIcon/>},
+    {name: 'Spring Framework', color: 'primary', logo: <SvgIcon component={SpringLogo} viewBox="0 0 97.1 97.1"/>},
+    {name: 'Spring Cloud', color: 'secondary', logo: <EmojiSymbolsIcon/>}
+]
+const random = randomLcg()
+const shuffle = shuffler(random);
+shuffle(skillsData)
+
 const useStyles = makeStyles(theme => ({
     skillItemChips: {
         display: 'flex',
         flexWrap: "wrap",
         justifyContent: 'center'
+    },
+    letterSkillLogo: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+        // textTransform: 'uppercase'
     }
 }));
 
 function Skills() {
 
-    const {skillItemChips} = useStyles()
+    const {skillItemChips, letterSkillLogo} = useStyles()
 
     return (
         <Fragment>
             <UniformCard style={{backgroundColor: cyan[300], height: 200}}>
                 <CardContent className={skillItemChips}>
-                    <SkillItemChip
-                        skillIcon={<EmojiSymbolsIcon/>}
-                        skillName='Microservice Architecture'
-                        color='secondary'
-                    />
-                    <SkillItemChip
-                        skillIcon={<CloudDoneIcon/>}
-                        skillName='Kubernetes'
-                        color='primary'
-                    />
-                    <SkillItemChip
-                        skillIcon={<CropIcon/>}
-                        skillName='Node.js'
-                        color='default'
-                    />
-                    <SkillItemChip
-                        // skillIcon={<CloudDoneIcon/>}
-                        skillIcon={<SvgIcon component={SpringLogo} viewBox="0 0 97.1 97.1"/>}
-                        skillName='Spring Framework'
-                        color='primary'
-                    />
-                    <SkillItemChip
-                        skillIcon={<EmojiSymbolsIcon/>}
-                        skillName='Spring Cloud'
-                        color='secondary'
-                    />
+                    {skillsData.map(({name, color, logo}) => {
+                        if (!logo)
+                            logo = <Avatar className={letterSkillLogo}>{name.charAt(0)}</Avatar>
+
+                        return <SkillItemChip
+                            key={name}
+                            skillIcon={logo}
+                            skillName={name}
+                            color={color}
+                        />
+                    })}
                 </CardContent>
             </UniformCard>
             <UniformCard style={{backgroundColor: cyan[100], height: 200}}>
