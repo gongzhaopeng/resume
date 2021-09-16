7dea2d81b86852c9473d9fa09915d356dca590e2
gongzhaopeng <gongguochuanqi@gmail.com>
2021-09-16 11:27:47 +0800

    ...

--- src/components/SkillDetailDialog.js
+++ src/components/SkillDetailDialog.js
@@ -5,19 +5,19 @@ import DialogTitle from '@material-ui/core/DialogTitle'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import Typography from '@material-ui/core/Typography'
-import Toolbar from '@material-ui/core/Toolbar'
 import CodeIcon from '@material-ui/icons/Code'
 import Rating from '@material-ui/lab/Rating'
+import Grid from '@material-ui/core/Grid'
 
 import {makeStyles} from '@material-ui/core/styles';
-import {yellow, cyan} from '@material-ui/core/colors';
+import {yellow, cyan, brown} from '@material-ui/core/colors';
 
 const useStyles = makeStyles(theme => ({
     header: {
         height: 50,
         backgroundColor: yellow[600],
         margin: 0,
-        padding: 0
+        padding: 10
     },
     titleText: {
         color: cyan[800],
@@ -27,7 +27,9 @@ const useStyles = makeStyles(theme => ({
     },
     content: {
         height: 500,
-        backgroundColor: yellow[50]
+        backgroundColor: yellow[50],
+        padding: 10,
+        color: brown[300]
     }
 }))
 
@@ -39,22 +41,33 @@ export function SkillDetailDialog({skillIcon, skillName, onClose}) {
         <Dialog open={true} onClose={onClose} scroll='paper'
                 maxWidth='sm' fullWidth>
             <DialogTitle className={header}>
-                <Toolbar>
-                    {skillIcon}
-                    <Typography className={titleText}>
-                        {skillName}
-                    </Typography>
-                </Toolbar>
+                <Grid container spacing={0}>
+                    <Grid item xs={1} align='center'>
+                        {skillIcon}
+                    </Grid>
+                    <Grid item xs={10} align='center'>
+                        <Typography className={titleText}>
+                            {skillName}
+                        </Typography>
+                    </Grid>
+                </Grid>
             </DialogTitle>
             <DialogContent dividers className={content}>
-                <Rating
-                    name="FAMILIARITY"
-                    readOnly
-                    defaultValue={3.5}
-                    precision={0.5}
-                    icon={<CodeIcon color='secondary'/>}
-                    emptyIcon={<CodeIcon/>}
-                />
+                <Grid container spacing={0}>
+                    <Grid item xs={6}>
+                        <Typography variant='button'>familiarity</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Rating
+                            name="FAMILIARITY"
+                            readOnly
+                            defaultValue={3.5}
+                            precision={0.5}
+                            icon={<CodeIcon color='secondary'/>}
+                            emptyIcon={<CodeIcon/>}
+                        />
+                    </Grid>
+                </Grid>
                 <DialogContentText>
                     KEEP GOING
                 </DialogContentText>
