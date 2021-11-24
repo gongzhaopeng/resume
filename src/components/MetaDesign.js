ba4c9c9a0e7b22b082d6ca7186d8673deb0f88d2
gongzhaopeng <gongguochuanqi@gmail.com>
2021-11-24 22:25:07 +0800

    ...

--- src/components/MetaDesign.js
+++ src/components/MetaDesign.js
@@ -21,12 +21,14 @@ const designDgsData = [
     {
         'title': 'mindmap.product.backlog.resume.atzu68.com',
         'image': 'https://lucid.app/publicSegments/view/cc415a13-e213-4437-a9f0-2b59fad6954a/image.png',
-        'url': 'https://lucid.app/documents/embeddedchart/9b620cab-59e8-49e6-b0f2-55b649bd2e5b?invitationId=inv_780ba4e6-abfc-4d12-ba9b-5f6b2f150fac#'
+        // 'url': 'https://lucid.app/documents/embeddedchart/9b620cab-59e8-49e6-b0f2-55b649bd2e5b?invitationId=inv_780ba4e6-abfc-4d12-ba9b-5f6b2f150fac#',
+        'scaleUpRatio': 2,
     },
     {
         'title': 'gen-info.me.mindmap.pb.resume.atzu68.com',
         'image': 'https://lucid.app/publicSegments/view/df1eac74-60b0-40aa-82d1-afa68412df80/image.png',
-        'url': 'https://lucid.app/documents/embeddedchart/276d6d0b-eace-4b85-8141-998e519d7017?invitationId=inv_3fbc92bf-a2c7-45d3-b2f3-9b4b6606e7c0#'
+        // 'url': 'https://lucid.app/documents/embeddedchart/276d6d0b-eace-4b85-8141-998e519d7017?invitationId=inv_3fbc92bf-a2c7-45d3-b2f3-9b4b6606e7c0#',
+        'scaleUpRatio': 2,
     },
     {
         'title': 'main.wireframe.ui.resume.atzu68.com',
