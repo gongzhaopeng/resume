14c052604f9be499e77540b79009e395ae2a3a7f
gongzhaopeng <gongguochuanqi@gmail.com>
2022-02-07 01:24:48 +0800

    ...

--- src/components/profile/ProfileFamilyCard.js
+++ src/components/profile/ProfileFamilyCard.js
@@ -11,12 +11,19 @@ import {lime} from '@material-ui/core/colors';
 import UniformCard from '../UniformCard';
 import SingleLineImageList from "../SingleLineImageList";
 
-import journeyToMarsImage from "../../assets/images/journeyToMars.jpeg"
+import iTheThreeOfUs from "../../assets/images/the-three-of-us/i.the-three-of-us.jpeg"
+import iiTheThreeOfUs from "../../assets/images/the-three-of-us/ii.the-three-of-us.jpeg"
+import iiiTheThreeOfUs from "../../assets/images/the-three-of-us/iii.the-three-of-us.jpeg"
+import ivTheThreeOfUs from "../../assets/images/the-three-of-us/iv.the-three-of-us.jpeg"
 
 const runnerImageItems = [{
-    img: journeyToMarsImage, title: "Journey to Mars"
+    img: iTheThreeOfUs, title: "i.the-three-of-us"
 }, {
-    img: journeyToMarsImage, title: "Journey to Mars"
+    img: iiTheThreeOfUs, title: "ii.the-three-of-us"
+}, {
+    img: iiiTheThreeOfUs, title: "iii.the-three-of-us"
+}, {
+    img: ivTheThreeOfUs, title: "iv.the-three-of-us"
 },]
 
 const useStyles = makeStyles((theme) => ({
@@ -34,7 +41,7 @@ export default function ProfileFamilyCard() {
         <CardHeader title={t("me:genInfo.family.slogan")}
                     titleTypographyProps={{variant: "button", color: "secondary", align: "center"}}/>
         <CardContent>
-            <SingleLineImageList imageItems={runnerImageItems} imgHeight={200}/>
+            <SingleLineImageList imageItems={runnerImageItems} imgHeight={242}/>
         </CardContent>
     </UniformCard>)
 }
